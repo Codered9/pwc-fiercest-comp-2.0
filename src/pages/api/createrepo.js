834204f740eff200d5626cf5891668a71e52b501
Sudhanshu Srivastava <sudhanshusri23@gmail.com>
2023-09-12 15:35:32 +0530

    Error logging

--- src/pages/api/createrepo.js
+++ src/pages/api/createrepo.js
@@ -1,20 +1,14 @@
 const {exec} = require("child_process")
 export default function handler(req, res) {
     const {repo_loc, repo_name , archive_name , source,  passphrase } = req.body.data
-    // const repo_loc = "/home/sudhanshu"
-    // const repo_name = "test84"
-    // const archive_name = "backend_test"
-    // const source = "/home/sudhanshu/test"
-    // const passphrase = "test1234"
     exec(`bash ./scripts/borg_create.sh ${repo_loc} ${repo_name} ${passphrase} ${source} ${archive_name}`, (err,stdout,stderr) => {
-    // exec(`pwd`, (err,stdout,stderr) => {
         if (err) {
             console.log(`error: ${err.message}`);
-            return;
+            res.status(500).json({error: stderr})
         }
         if (stderr) {
             console.log(`stderr: ${stderr}`);
-            return;
+            res.status(500).json({error: stderr})
         }
         console.log(`stdout: ${stdout}`);
     })
--- src/pages/api/restrorepo.js
+++ src/pages/api/restrorepo.js
@@ -1,18 +1,21 @@
-const {exec} = require("child_process")
+const { exec } = require("child_process");
 export default function handler(req, res) {
-    console.log(req.body.data)
-    const {repo_loc, repo_name , archive_name , dest,  passphrase } = req.body.data
-    exec(`bash ./scripts/borg_extract.sh ${repo_loc} ${repo_name} ${dest}  ${archive_name} ${passphrase}`, (err,stdout,stderr) => {
-    // exec(`pwd`, (err,stdout,stderr) => {
-        if (err) {
-            console.log(`error: ${err.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    })
-    res.status(200).json({ message: 'Backup generated successfully' })
-  }
+  console.log(req.body.data);
+  const { repo_loc, repo_name, archive_name, dest, passphrase } = req.body.data;
+  exec(
+    `bash ./scripts/borg_extract.sh ${repo_loc} ${repo_name} ${dest}  ${archive_name} ${passphrase}`,
+    (err, stdout, stderr) => {
+      // exec(`pwd`, (err,stdout,stderr) => {
+      if (err) {
+        console.log(`error: ${err.message}`);
+        res.status(500).json({ error: err });
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        res.status(500).json({ error: stderr });
+      }
+      console.log(`stdout: ${stdout}`);
+    }
+  );
+  res.status(200).json({ message: "Backup restored successfully" });
+}
